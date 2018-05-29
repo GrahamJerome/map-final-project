215b6471ad6d2b696f675360c6a52ad2be4545e9
Graham Jerome <graham.jerome@gmail.com>
2018-05-29 15:35:06 -0400

    Implemented foursquare API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { GoogleApiWrapper } from 'google-maps-react';
+
 import LocationsList from './LocationsList';
 import './css/App.css';
 
@@ -7,15 +8,30 @@ class App extends Component {
 	state = {
 		markers: [],
 		locations: [
-			{ title: "Freshii", position: { lat: 45.3539349, lng: -75.9341215 } },
+			{ title: "Freshii", position: { lat: 45.353835, lng: -75.931967 } },
 			{ title: "Shawarma House", position: { lat: 45.3578524, lng: -75.9358577 } },
 			{ title: "Willy's Pizza", position: { lat: 45.3541745, lng: -75.9354016 } },
 			{ title: "Starbucks", position: { lat: 45.3582155, lng: -75.9368827 } },
 			{ title: "McDonald's", position: { lat: 45.3587677, lng: -75.9386384 } }
-		]
+		],
+		foursquare: require('react-foursquare')({
+		  clientID: '4UXJZBCVXFLJUVGLKEX1VQVMCEFJDKJKYYASDHJEHH0WOK4I',
+		  clientSecret: 'BATMM0DKAH2ILJVRPXUWZ2WTSXLOU2V1V1S2P245AQ1DRBJI'
+		}),
+		fsItems: []
 	}
 
 	componentDidMount() {
+		const { locations, foursquare, fsItems } = this.state;
+		const fsParams = locations.map((location) => {
+			foursquare.venues.getVenues({
+				"ll": `${location.position.lat}, ${location.position.lng}`,
+				"query": location.title
+			}).then(res => {
+					Object.assign(location, res.response.venues[0]);
+			});
+		});
+
 		this.loadMap();
 	}
 
@@ -29,17 +45,16 @@ class App extends Component {
 		/* initialize the map */
 
 		const { google } = this.props;
-		const { locations, markers } = this.state;
-
+		const { locations, markers, fsItems } = this.state;
 		const mapNode   = document.getElementById('map');
 		const mapConfig = Object.assign({}, {
 			zoom     : 13,
 			center   : { lat: 45.3544903, lng: -75.9245926 },
 			mapTypeId: 'roadmap'
 		});
+		const bounds = new google.maps.LatLngBounds();
 
 		this.map = new google.maps.Map(mapNode, mapConfig);
-		const bounds = new google.maps.LatLngBounds();
 		this.mapInfoWindow = new google.maps.InfoWindow();
 
 		/* setup map markers */
@@ -51,7 +66,8 @@ class App extends Component {
 				position : location.position,
 				title    : location.title,
 				animation: google.maps.Animation.DROP,
-			  id: i
+			  id: i,
+			  fSquareInfo: location
 			});
 
 			// create an onclick event to open an infowindow at each marker.
@@ -70,16 +86,22 @@ class App extends Component {
 	}
 
 	populateInfoWindow(marker) {
-		const { google } = this.props;
-		// const mapInfoWindow = new google.maps.InfoWindow();
-
-		console.log(marker);
-
     // check to make sure the mapInfoWindow is not already opened on this marker.
-    if (this.mapInfoWindow.marker != marker) {
+    if (this.mapInfoWindow.marker !== marker) {
 	    this.mapInfoWindow.marker = marker;
-	    this.mapInfoWindow.setContent('<div>' + marker.title + '</div>');
-	    // this.mapInfoWindow.setPosition(marker.position);
+
+	    // populate the marker content
+	    this.mapInfoWindow.setContent(
+	    	`<div className="marker">` +
+	    		`<h2><img src="${marker.fSquareInfo.categories[0].icon.prefix+'bg_32'+marker.fSquareInfo.categories[0].icon.suffix}"> ${marker.fSquareInfo.name}</h2>` +
+	    		`<h4>${marker.fSquareInfo.categories[0].name}, <u>${marker.fSquareInfo.hereNow.summary}</u></h4>` +
+	    		`<p>checkins: <strong>${marker.fSquareInfo.stats.checkinsCount}</strong> tips: <strong>${marker.fSquareInfo.stats.tipCount}</strong> visits: <strong>${marker.fSquareInfo.stats.visitsCount}</strong></p>` +
+	    		`<div className="address">` +
+	    			`<p>${marker.fSquareInfo.location.formattedAddress[0]}</p>` +
+	    			`<p>${marker.fSquareInfo.location.formattedAddress[1]}</p>` +
+	    		`</div>` +
+	    	`</div>`
+	    );
 	    this.mapInfoWindow.open(this.map, marker);
 
 	    // make sure the marker property is cleared if the mapInfoWindow is closed.
